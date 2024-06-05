1942b1d9a5dd88f631078637c29155471b32fc6f
Yocary_CD <90988723+yocary@users.noreply.github.com>
2024-06-04 22:31:28 -0600

    Se modifica configuración
    Se modifica configuración

--- src/main/java/com/turnos/Application.java
+++ src/main/java/com/turnos/Application.java
@@ -5,11 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("com.turnos")
 public class Application {
     public static void main(String[] args) {
+        // Establecer la zona horaria del servidor a Guatemala
+        System.setProperty("user.timezone", "America/Guatemala");
+
         SpringApplication.run(Application.class, args);
+
+        LocalDateTime horaActual = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String horaFormateada = horaActual.format(formato);
+
+        System.out.println("La hora actual en Guatemala es: " + horaFormateada);
     }
 }
