7fcd59725731194191335aea9c6d5b56ee77d6d3
Yocary_CD <90988723+yocary@users.noreply.github.com>
2024-06-04 22:47:22 -0600

    Se hacen cambios en configuración
    Se hacen cambios en configuración

--- src/main/java/com/turnos/Application.java
+++ src/main/java/com/turnos/Application.java
@@ -1,39 +1,39 @@
+package com.turnos;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
 import java.util.TimeZone;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("com.turnos")
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public void setDefaultTimeZone() {
-        // Establecer la zona horaria por defecto para la aplicación
+    @PostConstruct
+    public void init() {
+        // Establecer la zona horaria por defecto
         TimeZone.setDefault(TimeZone.getTimeZone("America/Guatemala"));
-
-        // Imprimir la hora actual en la zona horaria de Guatemala
+        logger.info("La zona horaria por defecto se ha establecido a: America/Guatemala");
         imprimirHoraActual();
     }
 
     private void imprimirHoraActual() {
         // Obtener la hora actual en la zona horaria de Guatemala
-        LocalDateTime horaActual = LocalDateTime.now(ZoneId.of("America/Guatemala"));
-        // Formatear la hora actual
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String horaFormateada = horaActual.format(formato);
+        long horaActual = System.currentTimeMillis();
         // Imprimir la hora actual en la consola
-        System.out.println("La hora actual en Guatemala es: " + horaFormateada);
+        logger.info("La hora actual en Guatemala (en milisegundos) es: " + horaActual);
     }
 }
