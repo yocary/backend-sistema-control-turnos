0db9393498762bd3ea386c51d55908978874913b
Yocary_CD <90988723+yocary@users.noreply.github.com>
2024-06-04 22:39:18 -0600

    Se modifica configuración
    Se modifica configuración

--- src/main/java/com/turnos/Application.java
+++ src/main/java/com/turnos/Application.java
@@ -1,27 +1,39 @@
-package com.turnos;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("com.turnos")
 public class Application {
+
     public static void main(String[] args) {
-        // Establecer la zona horaria del servidor a Guatemala
-        System.setProperty("user.timezone", "America/Guatemala");
-
         SpringApplication.run(Application.class, args);
+    }
 
-        LocalDateTime horaActual = LocalDateTime.now();
+    @Bean
+    public void setDefaultTimeZone() {
+        // Establecer la zona horaria por defecto para la aplicación
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Guatemala"));
+
+        // Imprimir la hora actual en la zona horaria de Guatemala
+        imprimirHoraActual();
+    }
+
+    private void imprimirHoraActual() {
+        // Obtener la hora actual en la zona horaria de Guatemala
+        LocalDateTime horaActual = LocalDateTime.now(ZoneId.of("America/Guatemala"));
+        // Formatear la hora actual
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String horaFormateada = horaActual.format(formato);
-
+        // Imprimir la hora actual en la consola
         System.out.println("La hora actual en Guatemala es: " + horaFormateada);
     }
 }
